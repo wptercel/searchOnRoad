c205ad5c66147f66673132e577e5dbc6039cc6c6
Peng Wu <pwu@Pengs-MacBook-Pro.local>
2016-07-18 23:23:33 -0700

    Initial commit

---
+++ src/main/java/com/expedia/searchonroad/proxy/GoogleMapsDirectionProxy.java
@@ -0,0 +1,26 @@
+package com.expedia.searchonroad.proxy;
+
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.MediaType;
+
+public class GoogleMapsDirectionProxy {
+	private final String BASE_URL = "https://maps.googleapis.com/maps/api/directions";
+	private final String AUTHORIZATION = "AIzaSyAT_D6yyyYMmwttda35cImLQcqF1QGV1s0";
+
+	public void getDirection(String origin, String destination) {
+		Client client = ClientBuilder.newClient();
+		WebTarget target = client.target(BASE_URL).path("json")
+				.queryParam("origin", origin)
+				.queryParam("destination", destination)
+				.queryParam("key", AUTHORIZATION);
+
+		System.out.println(target.request(MediaType.TEXT_PLAIN_TYPE).get().readEntity(String.class));
+	}
+
+	public static void main(String args[]) {
+		GoogleMapsDirectionProxy test = new GoogleMapsDirectionProxy();
+		test.getDirection("Montreal", "Toronto");
+	}
+}
